ed32390e2ce00b9043948fe0d2c1772e1802aa22
mazerlodge <mazerlodge@hotmail.com>
2024-10-12 18:16:14 -0500

    Add time string to decimal conversion

--- src/echoNumLines.py
+++ src/echoNumLines.py
@@ -2,9 +2,28 @@
 
 datafile = open("./data/log1011_mac.txt", "r") 
 
+def getDecimalTime(timeString):
+	# Given a 4 digit time string, e.g. 1145 or 1430, 
+	#   return a decimal value, e.g. 11.75 or 14.5
+	rval = -1.0
+
+	# confirm the time provided is numeric and length 4
+	bValidTime = False 
+	if (timeString.isnumeric() and len(timeString) == 4):
+		bValidTime = True
+	else:
+		return(rval) 	
+	
+	# convert the 4 digit time to a decimal value 
+	rval = int(timeString[0:2]) + int(timeString[2:4])/60
+
+	return(rval) 
+
+
 for aLine in datafile:
 	timeStamp = aLine[0:4]
 	if (timeStamp.isnumeric()):
-		print(timeStamp)
+		decimalTime = getDecimalTime(timeStamp)
+		print(f"{timeStamp} aka {decimalTime}")
 
 datafile.close() 
